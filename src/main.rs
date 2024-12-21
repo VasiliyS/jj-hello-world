048d28a38fbe992ceb1e7262c57ce7919aec84fe
Vasily Suvovor <vsuvorov@gmail.com>
2024-12-21 18:46:16 +0100

    create hello and goodbye functions

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,14 @@
 
  /// adding a silly comment, just for the sake of it :)
 fn main() {
+  print_hello();
+  print_bye();
+}
+
+fn print_hello() {
     println!("Hello, world!");
+}
+
+fn print_bye() {
+    println!("Goodbye, world!");
 }
