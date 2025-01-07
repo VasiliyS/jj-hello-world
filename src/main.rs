c32df1351f2f1f424c7a67b4ad3d51d97227e28a
Vasily Suvovor <vsuvorov@gmail.com>
2025-01-07 18:05:41 +0100

    add a new function

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,8 @@
 /// The main function runs when our program starts
 fn main() {
   print("Hello, world!");
-}
+  print("Goodbye, world!");
+ }
 
 // a function that prints a message
 fn print(m: &str) {
